62a063108fb10dd4030434eef3e936bdde9ee498
Marlon Secundo <marlon_secundo@outlook.com>
2018-06-17 11:54:49 -0300

    Get Data of Feed XML

--- components/PostViewer.js
+++ components/PostViewer.js
@@ -11,7 +11,7 @@ class PostItem {
     content = "";
 
     constructor(id, title, content) {
-        this.id = id; 
+        this.id = id;
         this.title = title;
         this.content = content;
 
@@ -20,24 +20,53 @@ class PostItem {
 
 export class PostViewer extends Component {
 
-    render() {
-        return (
-            <View style={styles.container}>  
-                <FlatList keyExtractor={data => data.id} data={this.getDataList()} renderItem={this._renderItem} />
-            </View>
-        );
+    text = "";
+
+    componentDidMount = () => {
+        this.getDOOMFeed();
     }
+
+    getDOOMFeed = () => {
+        let request = new XMLHttpRequest();
+        request.open("GET", "https://seuserhumano.wordpress.com/feed/", true);
+        let response = "";
+
+        request.onreadystatechange = () => {
+            if (request.readyState == 4 && request.status == 200) {
+                response = request.responseText.replace("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", '');
+
+                window.DOMParser = require('xmldom').DOMParser;
+                this.extractPostRSS(new DOMParser().parseFromString(response, "text/xml"))
+            }
+        };
+
+        request.send(null);
+    }
+
+    extractPostRSS = (xmlDoc) => {
+        var desc = xmlDoc.getElementsByTagName("item")[0].getElementsByTagName("title")[0].textContent;
+
+    }
+
+
 
 
     getDataList = () => {
 
-        let items = [new PostItem("0", "Titulo 1" , "Conteudo Conteudo Conteudo Conteudo Conteudo Conteudo"), new PostItem("1", "Título 2", "Conteudo"), new PostItem("2", "Título 3", "Conteudo")];
+        let items = [new PostItem("0", "Titulo 1", "Conteudo Conteudo Conteudo Conteudo Conteudo Conteudo"), new PostItem("1", "Título 2", "Conteudo"), new PostItem("2", "Título 3", "Conteudo")];
 
         return items;
     }
 
     _renderItem = ({ item }) => (<Post title={item.title} content={item.content}></Post>);
 
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList keyExtractor={data => data.id} data={this.getDataList()} renderItem={this._renderItem} />
+            </View>
+        );
+    }
 }
 
 
